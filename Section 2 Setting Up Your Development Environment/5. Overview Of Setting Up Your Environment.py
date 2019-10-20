7ff5cc4981e6713cebc36a1409c49bd29c93a519
Grey <sergejyurskyj@yahoo.com>
2019-10-19 17:39:47 -0700

    Commit 5. Overview Of Setting Up Your Environment

---
+++ Section 2 Setting Up Your Development Environment/5. Overview Of Setting Up Your Environment.py
@@ -0,0 +1,11 @@
+import pymysql
+
+import cgi
+
+print('Content-type:text/html\n')
+print('<html>')
+print('<head><title>Hello World</title></head')
+print('<body>')
+print('<h2>Hello World.</h2')
+print('<body>')
+print('</html')
